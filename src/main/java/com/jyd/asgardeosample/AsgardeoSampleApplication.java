ef959f2c0c9885393ec50a3c39b516ab19b05a58
jayodkavinda <jayodkavinda@gmail.com>
2022-03-03 11:28:41 +0530

    init commit

---
+++ src/main/java/com/jyd/asgardeosample/AsgardeoSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jyd.asgardeosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsgardeoSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsgardeoSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyd/asgardeosample/AsgardeoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyd.asgardeosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsgardeoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
