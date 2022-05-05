b6f1fc0a92f8969e8b0ad447052956d3ce0d89e6
jayodkavinda <jayodkavinda@gmail.com>
2022-05-05 11:05:12 +0530

    add auth page and get user method

--- src/main/java/com/jyd/asgardeosample/AppController.java
+++ src/main/java/com/jyd/asgardeosample/AppController.java
@@ -5,12 +5,18 @@ import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 public class AppController {
 
-
-
+    @GetMapping("/auth")
+    public String authPage(Model model){
+        return "auth";
+    }
 
     @GetMapping("/")
     public String currentUserName(Model model, Authentication authentication) {
@@ -19,5 +25,11 @@ public class AppController {
         model.addAttribute("userName", userDetails.getName());
         model.addAttribute("IDTokenClaims", userDetails);
         return "home";
+    }
+
+    @RequestMapping("/user")
+    @ResponseBody
+    public Principal getUser(Principal principal){
+        return principal;
     }
 }
